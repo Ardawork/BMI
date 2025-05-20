c2665b239459cc63052514a74d03f5c36cd45dd3
Arda Karaca <ardakaraca2016@gmail.com>
2025-05-20 13:38:20 +0300

    weight section added

--- BMI.py
+++ BMI.py
@@ -5,5 +5,14 @@ window.title("BMI")
 window.minsize(400, 400)
 window.maxsize(400, 400)
 
+#Weight Section
+
+weight = tk.Label(text = "Weight" , font = "Arial")
+weight.place(x = 165, y = 100)
+
+enter_weight = tk.Entry(width = 20)
+enter_weight.place(x = 135, y = 150)
+#-------------------------------------------------------
+
 
 window.mainloop()
